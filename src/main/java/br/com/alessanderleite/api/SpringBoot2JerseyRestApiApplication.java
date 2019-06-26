9cd429dc7cc2525afbf1f2927aa59f6b7b14735d
alessanderleite <alessander.logistica@gmail.com>
2019-06-26 10:20:06 -0300

    initial commit

---
+++ src/main/java/br/com/alessanderleite/api/SpringBoot2JerseyRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alessanderleite.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2JerseyRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2JerseyRestApiApplication.class, args);
+	}
+
+}
